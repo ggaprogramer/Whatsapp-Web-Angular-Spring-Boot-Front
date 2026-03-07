1ea296f2247e15db9c938456ce82e090b85216e4
Guilherme Andrade <guilherme.g.andrade11@gmail.com>
2026-03-07 19:11:21 -0300

    Completando o primeiro rascunho do design da dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -12,4 +12,22 @@ import { CommonModule } from '@angular/common';
 })
 export class DashboardComponent {
 
+  @ViewChild('variableContainerConversationsHeaderOptions') variableContainerConversationsHeaderOptions!: ElementRef<HTMLButtonElement>;
+  @ViewChild('variableContainerConversationHeaderActionsOptions') variableContainerConversationHeaderActionsOptions!: ElementRef<HTMLButtonElement>;
+  @ViewChild('variableContainerConversationMessagesActionsAdd') variableContainerConversationMessagesActionsAdd!: ElementRef<HTMLButtonElement>;
+
+  toggleContainerConversationsHeaderOptions() {
+    this.variableContainerConversationsHeaderOptions
+    .nativeElement.children[1]!.classList.toggle('container-conversations_header_buttons_options--view');
+  }
+
+  toggleContainerConversationHeaderActionsOptions() {
+    this.variableContainerConversationHeaderActionsOptions
+    .nativeElement.children[1]!.classList.toggle('container-conversation_header_actions_options--view');
+  }
+
+  toggleContainerConversationMessagesActionsAdd() {
+    this.variableContainerConversationMessagesActionsAdd
+    .nativeElement.children[1]!.classList.toggle('container-conversation_messages_actions_add--view');
+  }
 }
